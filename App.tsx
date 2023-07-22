a4fbe0300d7188437e7a59447b18839ab52c66a8
waramity <waramity.re@gmail.com>
2023-07-23 04:58:12 +0700

    add Tensor Camera Screen, no bug, but can't use properly

--- App.tsx
+++ App.tsx
@@ -1,14 +1,33 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as tf from '@tensorflow/tfjs';
+
 import MainScreen from './screens/MainScreen';
 import DefaultCameraScreen from './screens/DefaultCameraScreen';
 import ObjectDetectionCameraScreen from './screens/ObjectDetectionCameraScreen';
 
 const Stack = createNativeStackNavigator();
 
+interface AppState {
+  isTfReady: boolean;
+}
+
 const App: React.FC = (): React.ReactElement => {
+const [isTfReady, setIsTfReady] = useState(false);
+	  useEffect(() => {
+    // Wait for tf to be ready.
+    const initializeTensorFlow = async () => {
+      await tf.ready();
+      // Signal to the app that tensorflow.js can now be used.
+      setIsTfReady(true);
+    };
+    initializeTensorFlow();
+  }, []);
+	console.log(isTfReady)
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Main">
--- screens/ObjectDetectionCameraScreen.tsx
+++ screens/ObjectDetectionCameraScreen.tsx
@@ -1,50 +1,130 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, StyleSheet, Platform, Text } from 'react-native';
 import CameraComponent from '../components/common/CameraComponent'
 
-import { Camera } from 'expo-camera';
+import { Camera, CameraType } from 'expo-camera';
 import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
+import { GLView, ExpoWebGLRenderingContext } from 'expo-gl';
+import * as tf from '@tensorflow/tfjs-core';
+
+
 
 const TensorCamera = cameraWithTensors(Camera);
 
-interface Props {
-  onReady: (
-    images: IterableIterator<tf.Tensor3D>,
-    updateCameraPreview: () => void,
-    gl: ExpoWebGLRenderingContext,
-    cameraTexture: WebGLTexture
-  ) => void;
-}
+export default function ObjectDetectionCameraScreen() {
+  const [hasPermission, setHasPermission] = useState<null | boolean>(null);
 
-const ObjectDetectionCameraScreen = () => {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
 
-function handleCameraStream(images, updatePreview, gl) {
-     const loop = async () => {
-       const nextImageTensor = images.next().value
-
-       //
-       // do something with tensor here
-       //
-
-       // if autorender is false you need the following two lines.
-       // updatePreview();
-       // gl.endFrameEXP();
-
-       requestAnimationFrame(loop);
-     }
-     loop();
-   }
-  return (
-    <View style={styles.container}>
-	  <CameraComponent />
-    </View>
-  );
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+	const textureDims = Platform.OS === 'ios' ?
+{
+  height: 1920,
+  width: 1080,
+} :
+{
+  height: 1200,
+  width: 1600,
 };
 
+  return (
+<View style={styles.container}>
+<TensorCamera 
+    style={styles.camera} 
+    onReady={() => {}}
+    resizeHeight={200}
+    resizeWidth={152}
+    resizeDepth={3}
+    autorender={true}
+    cameraTextureHeight={textureDims.height}
+    cameraTextureWidth={textureDims.width}
+          useCustomShadersToResize={false} // Add this property
+ /></View>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  camera:{
+    flex: 1,
+  },
 });
 
-export default ObjectDetectionCameraScreen;
+
+//class ObjectDetectionCameraScreen extends React.Component<{}> {
+//  handleCameraStream = (
+//    images: IterableIterator<tf.Tensor3D>,
+//    updatePreview: () => void,
+//    gl: WebGLRenderingContext
+//  ) => {
+//    const loop = async () => {
+//      const nextImageTensor = images.next().value;
+
+//      //
+//      // do something with tensor here
+//      //
+
+//      // if autorender is false you need the following two lines.
+//      // updatePreview();
+//      // gl.endFrameEXP();
+
+//      requestAnimationFrame(loop);
+//    };
+//    loop();
+//  };
+
+//  render() {
+//    // Currently expo does not support automatically determining the
+//    // resolution of the camera texture used. So it must be determined
+//    // empirically for the supported devices and preview size.
+
+//    let textureDims;
+//    if (Platform.OS === 'ios') {
+//      textureDims = {
+//        height: 1920,
+//        width: 1080,
+//      };
+//    } else {
+//      textureDims = {
+//        height: 1200,
+//        width: 1600,
+//      };
+//    }
+
+//    return (
+//      <View>
+//        <TensorCamera
+//          style={styles.camera}
+//          type={0}
+//          cameraTextureHeight={textureDims.height}
+//          cameraTextureWidth={textureDims.width}
+//          resizeHeight={200}
+//          resizeWidth={152}
+//          resizeDepth={3}
+//          onReady={this.handleCameraStream}
+//          useCustomShadersToResize={false} // Add this property
+//          autorender={true}
+//        />
+//      </View>
+//    );
+//  }
+//}
+
+//const styles = {
+//  camera: {
+//    // Your camera styles here
+//  },
+//};
+
+//export default ObjectDetectionCameraScreen;
