c52ea84127506f6c41064043aabe21f479a0137d
unknown <waramity.re@gmail.com>
2023-07-24 02:32:27 +0700

    install @tensorflow/tfjs-core and @tensorflow/tfjs-converter

--- screens/RealtimeObjectRecognizeScreen.tsx
+++ screens/RealtimeObjectRecognizeScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { View, StyleSheet, Platform, Text } from "react-native";
 import CameraComponent from "../components/common/CameraComponent";
 
@@ -14,7 +14,7 @@ export default function RealtimeObjectRecognizeScreen() {
   const [hasPermission, setHasPermission] = useState<null | boolean>(null);
   const [net, setNet] = useState<mobilenet.MobileNet>();
 
-  const load = async () => {
+  const loadModel = async () => {
     try {
       setNet(async () => await mobilenet.load());
       console.log("model loaded");
@@ -22,14 +22,52 @@ export default function RealtimeObjectRecognizeScreen() {
       console.log("model not loaded");
     }
   };
+
   useEffect(() => {
-    load();
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+
+      console.log("set permission");
+    })();
+    loadModel();
   }, []);
 
+  let frame = 0;
+  const computeRecognitionEveryNFrames = 60;
+
+  const handleCameraStream = (images: IterableIterator<tf.Tensor3D>) => {
+    console.log("handle camera stream is on");
+    const loop = async () => {
+      console.log("loop is stared");
+      if (net) {
+        console.log("net model is on");
+        if (frame % computeRecognitionEveryNFrames === 0) {
+          console.log("start compute every frame");
+          const nextImageTensor = images.next().value;
+          console.log("send next image tensor");
+          if (nextImageTensor) {
+            console.log("next image tensor is not null");
+            const objects = await net.classify(nextImageTensor);
+            console.log(objects.map((object) => object.className));
+            tf.dispose([nextImageTensor]);
+          }
+        }
+        frame += 1;
+        frame = frame % computeRecognitionEveryNFrames;
+      }
+
+      requestAnimationFrame(loop);
+    };
+    loop();
+  };
+
   if (hasPermission === null) {
+    console.log("permission is null");
     return <View />;
   }
   if (hasPermission === false) {
+    console.log("no permission");
     return <Text>No access to camera</Text>;
   }
   if (!net) {
@@ -47,28 +85,11 @@ export default function RealtimeObjectRecognizeScreen() {
           width: 1600,
         };
 
-  const handleCameraStream = (images: IterableIterator<tf.Tensor3D>) => {
-    const loop = async () => {
-      if (net) {
-        const nextImageTensor = images.next().value;
-        if (nextImageTensor) {
-          const objects = await net.classify(nextImageTensor);
-          console.log(objects.map((object) => object.className));
-          tf.dispose([nextImageTensor]);
-        }
-      }
-      requestAnimationFrame(loop);
-    };
-    loop();
-  };
-
   return (
     <View style={styles.container}>
       <TensorCamera
         style={styles.camera}
-        onReady={() => {
-          handleCameraStream();
-        }}
+        onReady={handleCameraStream}
         resizeHeight={200}
         resizeWidth={152}
         resizeDepth={3}
