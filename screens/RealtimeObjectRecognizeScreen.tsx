9b6799303d9055d1790390c6aeb2e99f0640da37
unknown <waramity.re@gmail.com>
2023-07-24 21:17:14 +0700

    show classified text in realtime object detection screen

--- screens/RealtimeObjectRecognizeScreen.tsx
+++ screens/RealtimeObjectRecognizeScreen.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, StyleSheet, Platform, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Platform,
+  Text,
+  ProgressBarAndroid,
+  ProgressViewIOS,
+} from "react-native";
 import CameraComponent from "../components/common/CameraComponent";
 
 import { Camera, CameraType } from "expo-camera";
@@ -11,9 +18,10 @@ import * as mobilenet from "@tensorflow-models/mobilenet";
 const TensorCamera = cameraWithTensors(Camera);
 
 export default function RealtimeObjectRecognizeScreen() {
+  const [loadingProgress, setLoadingProgress] = useState(0);
   const [hasPermission, setHasPermission] = useState<null | boolean>(null);
   const [net, setNet] = useState<mobilenet.MobileNet>();
-
+  const [classifiedText, setClassifiedText] = useState("Initial Text");
 
   const loadModel = async () => {
     console.log("Load model");
@@ -29,32 +37,39 @@ export default function RealtimeObjectRecognizeScreen() {
       setHasPermission(status === "granted");
 
       console.log("set permission");
-	  setNet(await mobilenet.load());
+      setNet(await mobilenet.load());
 
-    console.log("model loaded");
-
+      console.log("model loaded");
     })();
 
     loadModel();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoadingProgress((prevProgress) => {
+        if (prevProgress >= 0.9) {
+          clearInterval(interval);
+        }
+        return prevProgress + 0.1;
+      });
+    }, 500);
+
+    return () => clearInterval(interval);
   }, []);
 
   let frame = 0;
   const computeRecognitionEveryNFrames = 60;
 
   const handleCameraStream = (images: IterableIterator<tf.Tensor3D>) => {
-    console.log("handle camera stream is on");
     const loop = async () => {
-      console.log("loop is stared");
       if (net) {
-        console.log("net model is on");
         if (frame % computeRecognitionEveryNFrames === 0) {
-          console.log("start compute every frame");
           const nextImageTensor = images.next().value;
-          console.log("send next image tensor");
           if (nextImageTensor) {
-            console.log("next image tensor is not null");
             const objects = await net.classify(nextImageTensor);
             console.log(objects.map((object) => object.className));
+            setClassifiedText(objects.map((object) => object.className));
             tf.dispose([nextImageTensor]);
           }
         }
@@ -67,15 +82,36 @@ export default function RealtimeObjectRecognizeScreen() {
     loop();
   };
 
+  const renderProgressBar = () => {
+    // if (loadingProgress >= 1) {
+    //   // Model has been loaded, render your main component here
+    //   return <MainComponent net={net} />;
+    // }
+
+    if (Platform.OS === "ios") {
+      return <ProgressViewIOS progress={loadingProgress} />;
+    } else {
+      return (
+        <ProgressBarAndroid
+          styleAttr="Horizontal"
+          indeterminate={false}
+          progress={loadingProgress}
+        />
+      );
+    }
+  };
+
   if (hasPermission === null) {
     console.log("permission is null");
     return <View />;
   }
+
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
+
   if (!net) {
-    return <Text>Model not loaded</Text>;
+    return renderProgressBar();
   }
 
   const textureDims =
@@ -102,6 +138,7 @@ export default function RealtimeObjectRecognizeScreen() {
         cameraTextureWidth={textureDims.width}
         useCustomShadersToResize={false} // Add this property
       />
+      <Text style={styles.classifiedText}>{classifiedText}</Text>
     </View>
   );
 }
@@ -112,5 +149,8 @@ const styles = StyleSheet.create({
   },
   camera: {
     flex: 1,
+  },
+  classifiedText: {
+    height: 50,
   },
 });
