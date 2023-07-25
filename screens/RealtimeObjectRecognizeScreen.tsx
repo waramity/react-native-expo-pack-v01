6a1c4dd2ff3bbb5bb3d64a96dfbbbbf4c4ffc430
unknown <waramity.re@gmail.com>
2023-07-25 23:05:52 +0700

    change hasPermission == false in relatime object recogmnize screen

--- screens/RealtimeObjectRecognizeScreen.tsx
+++ screens/RealtimeObjectRecognizeScreen.tsx
@@ -35,11 +35,7 @@ export default function RealtimeObjectRecognizeScreen() {
     setClassifiedText(objects.map((object) => object.className));
   }
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-
-  if (hasPermission === false) {
+  if (hasPermission === false || hasPermission == null) {
     return <Text>No access to camera</Text>;
   }
 
